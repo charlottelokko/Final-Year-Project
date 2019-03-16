4f473216509e14bc58dc1189b190f7b1e2908ca4
Charlotte Lokko <littlelokko@hotmail.com>
2019-03-16 12:32:17 +0000

    trying to fix auto build

--- js/recorder.js
+++ js/recorder.js
@@ -1,29 +1,29 @@
-var audioCtx = new AudioContext(); 
-var dest = audioCtx.createMediaStreamDestination();
-var recorder = new MediaRecorder(dest.stream);
-let audio = document.getElementById('playBack');
+// var audioCtx = new AudioContext(); 
+// var dest = audioCtx.createMediaStreamDestination();
+// var recorder = new MediaRecorder(dest.stream);
+// let audio = document.getElementById('playBack');
 
-var rec = document.getElementById("rec");
-var stop = document.getElementById("stop");
-container.connect(dest);
-rec.onclick = function(){record()};
-stop.onclick = function(){stopRec()};
+// var rec = document.getElementById("rec");
+// var stop = document.getElementById("stop");
+// container.connect(dest);
+// rec.onclick = function(){record()};
+// stop.onclick = function(){stopRec()};
 
-const data = [];
+// const data = [];
 
-function record(){
-    //Start recording
-    recorder.start();
-}
+// function record(){
+//     //Start recording
+//     recorder.start();
+// }
 
-function stopRec(){
-    //stop recording
-    recorder.stop();
-}
+// function stopRec(){
+//     //stop recording
+//     recorder.stop();
+// }
 
-recorder.ondataavailable = evt => data.push(evt.data);
-recorder.onstop = evt => {
-    let blob = new Blob(data, {type: 'audio/ogg; codecs=opus'});
-    audio.src = URL.createObjectURL(blob);
-};
+// recorder.ondataavailable = evt => data.push(evt.data);
+// recorder.onstop = evt => {
+//     let blob = new Blob(data, {type: 'audio/ogg; codecs=opus'});
+//     audio.src = URL.createObjectURL(blob);
+// };
 
